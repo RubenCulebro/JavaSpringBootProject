6a4c8e9125cd7d057702d777e0996ab5d35fc964
Ruben Culebro <ruben.culebro_olvera68@ed.cna.nl.ca>
2023-11-28 10:46:48 -0330

    Part1 Basic Spring Framework

---
+++ src/main/java/com/example/javaspringbootproject/Course.java
@@ -0,0 +1,48 @@
+package com.example.javaspringbootproject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer courseId;
+    private String courseName;
+    private String courseNumber;
+    private Integer capacity;
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+}
